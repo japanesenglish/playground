3eb61e373cc08887c3f6884c43b69e06174ba436
japanesenglish <hashiyuu0622@gmail.com>
2024-12-07 22:12:15 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let op = document.getElementById('op');
+let li = document.querySelector('iframe');
+op.addEventListener('click',function(){
+    if(op.classList.contains('open') == false){
+        op.classList.toggle('open');
+        li.style.height = '400px';
+        li.style.width = '200px'
+    } else {
+        op.classList.toggle('open');
+        li.style.height = '';
+        li.style.width = '';
+    }
+    console.log('aaaaa');
+});
