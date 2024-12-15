475e68a08c58f4a3247f7031dfc31a708290a8a0
japanesenglish <hashiyuu0622@gmail.com>
2024-12-15 19:56:28 +0900

    Add files via upload

--- list/quiz/quiz.js
+++ list/quiz/quiz.js
@@ -7,9 +7,15 @@ acs.forEach(function(car){
         if(car.classList.contains('open') == false){
             car.classList.toggle('open');
             car.parentElement.querySelector('.acbox').style.height = car.parentElement.querySelector('.acbox').scrollHeight + 'px';
+            setTimeout(() => {
+                car.parentElement.querySelector('.acbox').style.height = 'auto';
+            }, 1000);
         } else {
             car.classList.toggle('open');
-            car.parentElement.querySelector('.acbox').style.height = '';
+            car.parentElement.querySelector('.acbox').style.height = car.parentElement.querySelector('.acbox').scrollHeight + 'px';
+            setTimeout(() => {
+                car.parentElement.querySelector('.acbox').style.height = '';
+            }, 1);
         };
     });
 });
@@ -19,9 +25,11 @@ choices.forEach(function(car){
     car.addEventListener('click',function(){
         if(car.classList.contains('correct')){
             car.querySelector('div').style.border = 'red solid 5px';
-            car.parentElement.parentElement.querySelector('.aca').style.height = car.parentElement.parentElement.querySelector('.aca').scrollHeight + 'px';
             setTimeout(() => {
-                car.parentElement.parentElement.style.height = car.parentElement.parentElement.scrollHeight + 'px';
+                car.parentElement.parentElement.querySelector('.aca').style.height = car.parentElement.parentElement.querySelector('.aca').scrollHeight + 'px';
+                setTimeout(() => {
+                    car.parentElement.parentElement.style.height = 'auto';
+                }, 1000);
             }, 1000);
         } else {
             car.querySelector('div').style.background = 'black';
